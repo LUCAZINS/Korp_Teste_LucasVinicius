aa66b703f98b8ac583fb48e58c0ccddbd0bbd8fa
LUCAZINS <lv998393623@gmail.com>
2026-04-19 17:17:41 -0300

    primeiro commit

---
+++ frontend/src/app/services/estoque.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Produto } from '../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EstoqueService {
+  private apiUrl = 'http://localhost:5001/api/produtos';
+
+  constructor(private http: HttpClient) { }
+
+  obterTodosProdutos(): Observable<Produto[]> {
+    return this.http.get<Produto[]>(this.apiUrl);
+  }
+
+  obterProdutoPorId(id: number): Observable<Produto> {
+    return this.http.get<Produto>(`${this.apiUrl}/${id}`);
+  }
+
+  criarProduto(produto: { codigo: number; descricao: string; saldo: number }): Observable<Produto> {
+    return this.http.post<Produto>(this.apiUrl, produto);
+  }
+
+  atualizarProduto(id: number, produto: { codigo: number; descricao: string; saldo: number }): Observable<Produto> {
+    return this.http.put<Produto>(`${this.apiUrl}/${id}`, produto);
+  }
+
+  deletarProduto(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
---
+++ frontend/src/app/services/faturamento.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { NotaFiscal, CriarNotaFiscalRequest } from '../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FaturamentoService {
+  private apiUrl = 'http://localhost:5002/api/notasfiscais';
+
+  constructor(private http: HttpClient) { }
+
+  obterTodasNotas(): Observable<NotaFiscal[]> {
+    return this.http.get<NotaFiscal[]>(this.apiUrl);
+  }
+
+  obterNotaPorId(id: number): Observable<NotaFiscal> {
+    return this.http.get<NotaFiscal>(`${this.apiUrl}/${id}`);
+  }
+
+  criarNotaFiscal(request: CriarNotaFiscalRequest): Observable<NotaFiscal> {
+    return this.http.post<NotaFiscal>(this.apiUrl, request);
+  }
+
+  imprimirNotaFiscal(id: number): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/${id}/imprimir`, {});
+  }
+
+  deletarNotaFiscal(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
